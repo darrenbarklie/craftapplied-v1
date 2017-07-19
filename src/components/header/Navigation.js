f369fb67d084829150b4f5174fae3a406a294c3c
darrenbarklie <darrenbarklie@gmail.com>
2017-07-19 21:07:28 +0700

    Prepared for test deployment

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -12,13 +12,13 @@ class Navigation extends Component {
 
           <li><Link to="services" spy={true} smooth={true} duration={500} activeClass="active">Services</Link></li>
 
-          <li><Link to="contact" spy={true} smooth={true} duration={500} activeClass="active">Contact</Link></li>
-
           <li><Link to="work" spy={true} smooth={true} duration={500} activeClass="active">Work</Link></li>
 
-          <li><a href="#">Blog</a></li>
+            <li><Link to="contact" spy={true} smooth={true} duration={500} activeClass="active">Contact</Link></li>
 
-          <li><a href="#">Project Planner</a></li>
+          {/* <li><a href="#">Blog</a></li>
+
+          <li><a href="#">Project Planner</a></li> */}
         </ul>
 
       </nav>
